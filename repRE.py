9f631062e1192821ffd7ba581c1cd64338fc0524
sonalishanbhag28 <88202408+sonalishanbhag28@users.noreply.github.com>
2021-08-20 15:55:29 +0530

    Add files via upload

---
+++ repRE.py
@@ -0,0 +1,22 @@
+import re
+from nltk.corpus import wordnet
+R_patterns = [
+    (r'won\'t', 'will not'),
+    (r'can\'t', 'cannot'),
+    (r'I\'m', 'I am'),
+    (r'Let\'s', 'Let us'),
+    (r'(\w+)\'d', '\g<1> would'),
+    (r'(\w+)\'ll', '\g<1> will'),
+    (r'(\w+)n\'t', '\g<1> not'),
+    (r'(\w+)\'ve', '\g<1> have'),
+    (r'(\w+)\'s', '\g<1> is'),
+    (r'(\w+)\'re', '\g<1> are'),
+]
+class REReplacer(object):
+    def __init__(self, patterns=R_patterns):
+        self.patterns = [(re.compile(regex), repl) for (regex, repl) in patterns]
+    def replace(self, text):
+        s = text
+        for (pattern, repl) in self.patterns:
+            s = re.sub(pattern, repl, s)
+        return s
